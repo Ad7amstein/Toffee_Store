999f89e2e5dfdd1b3cb1e6de79047fc46f4e5098
3llam <adham32003200@gmail.com>
2023-05-19 23:02:32 +0300

    remove app password

--- src/OTPManager.java
+++ src/OTPManager.java
@@ -43,7 +43,8 @@ public class OTPManager {
         // Create a session with email server
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, "ay7aga"); // replace with your email password
+                return new PasswordAuthentication(fromEmail, "app_password"); // replace with your email password
+                                                                                  // (app password)
             }
         });
 
